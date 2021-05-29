ebe6fa768de1a2a2a52199c9c59920bf904f183c
amr mohamed <amrmohamed362363@gmail.com>
2021-05-29 23:37:52 +0200

    modified

--- Headers/functions_prototype.h
+++ Headers/functions_prototype.h
@@ -1,1 +1,12 @@
-#pragma once
+#ifndef functions_prototype
+
+#define functions_prototype
+
+
+double degtorad(double deg);
+double calcDistance(double long1, double long2, double lat1, double lat2);
+
+
+#endif // !functions_prototype
+
+
