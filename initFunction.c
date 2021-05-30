b66f0fe78609dbf6b7d339003e1e8958ef7ac26f
amr mahmoud <amrmah785@gmail.com>
2021-05-31 01:36:18 +0200

    UART Intialization added

--- initFunction.c
+++ initFunction.c
@@ -34,6 +34,19 @@ void init_ports(){
 	GPIO_PORTD_AFSEL_R &= ~((1 << PORTD0) | (1 << PORTD1) | (1 << PORTD2));//disable alternate function
 	GPIO_PORTD_DEN_R |= ((1 << PORTD0) | (1 << PORTD1) | (1 << PORTD2));   //enable digital signals
 	//UART init 
+  SYSCTL_RCGCUART_R |= (0x0004);
+	UART2_CTL_R &= ~(0x0001);
+	UART2_IBRD_R |= 104;
+	UART2_FBRD_R |= 11;
+	UART2_LCRH_R &= ~(0x08); //Make FIFO have only one stop bit
+	UART2_LCRH_R |= 0x60; //Make Word Length 8 bits
+	GPIO_PORTD_LOCK_R = GPIO_LOCK_KEY; //Unlock PORT D
+	GPIO_PORTD_DIR_R &= ~((1 << PORTD6) | (1 << PORTD7)); //Enable PINS D6, D7 as inputs for GPS
+	GPIO_PORTD_AMSEL_R &= ~((1 << PORTD6) | (1 << PORTD7) ); //disable analog signals 
+	GPIO_PORTD_PCTL_R |= (0x11000000); 	//use PINS D6, D7 as UART Tx, Rx
+	GPIO_PORTD_AFSEL_R |= ((1 << PORTD6) | (1 << PORTD7) ));//enable alternate function
+	GPIO_PORTD_DEN_R |= ((1 << PORTD6) | (1 << PORTD7) ); //enable PINS D6, D7 as Digital pins
+	UART6_CTL_R |= (0x01); // Enable UART2 Tx, Rx
 
 
 
