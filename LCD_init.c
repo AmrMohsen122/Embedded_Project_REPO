45db14048921b51598b349099eaed4a0bc948da6
AmrAmin <1803092@eng.asu.edu.eg>
2021-05-30 14:48:01 +0200

    edited LCD_init.c

--- LCD_init.c
+++ LCD_init.c
@@ -1,25 +1,31 @@
-#inclue"tm4c123gh6pm.h"
+//LCD command function for setting portB(data) portD(command)
+//(d0 --> lcd register select rs d1 --> lcd read/write RW d2 --> lcd enable)
 void LCD_comm(char c){
-	GPIO_PORTD_DATA_R = O;
+	GPIO_PORTD_DATA_R &= ~0x07;
 	GPIO_PORTB_DATA_R = c;
-	GPIO_PORTD_DATA_R |= 0x80;
-	delay(230);
+	GPIO_PORTD_DATA_R |= 0x04;
+	delay(230)
 	GPIO_PORTD_DATA_R &= 0;
 }
+//initializing portB and portD(DO~D02)
 void LCD_init(){
-	SYSCTL_RCGCGPIO_R |=0xA;
-	GPIO_PORTB_DIR_R = 0xff;
-	GPIO_PORTB_DEN_R = 0Xff;
-	GPIO_PORTB_AFSEL_R &= 0;
-	GPIO_PORTB_AMSEL_R &= 0;
-	GPIO_PORTD_DIR_R |= 0x7;
-	GPIO_PORTD_DEN_R |= 0x7;
-	GPIO_PORTD_AFSEL_R &= 0;
-	GPIO_PORTD_AMSEL_R &= 0;
-	LCD_comm(0x30);
-	delay(200);
-	LCD_comm(0x06);
-	delay(200);
-	LCD_comm(0x0f);
-	delay(200);
+	SYSCTL_RCGCGPIO_R |=0xA;//clack initiation portB,portD
+	GPIO_PORTB_DIR_R = 0xff;//direction init for portB
+	GPIO_PORTB_DEN_R = 0Xff;//digital enable portB
+	GPIO_PORTB_AFSEL_R = 0;//disable analog function
+	GPIO_PORTB_AMSEL_R = 0;
+	GPIO_PORTD_DIR_R |= 0x7;//direction init for portD(D0~D2)
+	GPIO_PORTD_DEN_R | = 0x7;//digital enable for portD(D0~D2)
+	GPIO_PORTD_AFSEL_R &= ~0x07;//disable analog function
+	GPIO_PORTD_AMSEL_R &= ~0x07;
+	LCD_comm(0x30);//LCD wakeup
+	delay(20);
+	LCD_comm(0x38);//enabling 2 line deisplay
+	delay(20);
+	LCD_comm(0x01);//clear display
+	delay(20);
+	LCD_comm(0x06);//cursor increment
+	delay(20);
+	LCD_comm(0x0f);//display on
+	delay(20);
 }
