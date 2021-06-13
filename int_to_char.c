2a1b1733dfede33b21fb2c251f11efb3a8e2e2e6
amr mahmoud <amrmah785@gmail.com>
2021-06-13 19:53:01 +0200

    Added Function to change distance into a string for printing on LCD

--- int_to_char.c
+++ int_to_char.c
@@ -12,3 +12,23 @@ void reverseArray(char *arr, char *reversed) {
     }
    
 }
+
+char *int_to_char(int x){
+	int i;
+	char arr[5];
+	static char reversed[5];
+	if(x == 0){
+		arr[0] = '0';
+		arr[1] = '\0';
+	}
+	for(i = 0 ; x != 0; i++){
+		arr[i] = (int) (x % 10) + 48;
+		x /= 10;
+		if(x == 0){
+			arr[i+1] = '\0';
+		}
+	
+	}
+	reverseArray(arr,reversed);
+	return reversed;	
+}
