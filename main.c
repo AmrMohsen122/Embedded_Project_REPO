2c4d485db316520320a2e64b2e120fa44e3f60d5
AmrAmin <1803092@eng.asu.edu.eg>
2021-06-13 19:34:28 +0200

    added lcd init and recieved first coord from gps

--- main.c
+++ main.c
@@ -1,12 +1,25 @@
 #include "Headers/functions_prototype.h"
 #include "Headers/GPIO_DEF.h"
+#include "Headers/tm4c123gh6pm.h"
+#include <stdlib.h>
 int main(void)
 {
-    init_ports();
-    delay_ms(50);
-    LCD_init();
-    delay_ms(20);
-    LCD_display_string("Hello 123");
+	int i;
+	double amount = 0;
+	double distance = 0;
+	int cont = 1;
+	char *arr = (char *)malloc(100 * sizeof(char));
+	double coordinates[2][2];
+	init_ports();
+	delay_ms(50);
+	LCD_init();
+	delay_ms(20);
+	while(cont){
+		get_nema_string(arr , &cont);
+	}
+	cont = 1;
+	getPosition(arr , coordinates[0]);
+	delay_ms(2000);
     while (1)
     {
         double distance = 102;
