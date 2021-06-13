24bc9de7fe223585f5b07141abcd6e47fb198cca
AmrMohsen122 <amrmohsen2000@hotmail.com>
2021-06-13 19:41:39 +0200

    added body of while loop

--- main.c
+++ main.c
@@ -22,9 +22,23 @@ int main(void)
 	delay_ms(2000);
     while (1)
     {
-        double distance = 102;
-        REDLed(distance);
-    }
+        		while(cont){
+				get_nema_string(arr , &cont);
+			}
+			cont = 1;
+			if((GPIO_PORTF_DATA_R & ~0x01) ==0){break;}
+			getPosition(arr , coordinates[1]);
+			calcDistance(coordinates[0][1] , coordinates[1][1] , coordinates[0][0], coordinates[1][0] , &amount);
+			distance += amount;
+			//swap the two points
+			for(i = 0 ; i < 2 ; i++){
+				coordinates[0][i] = coordinates[1][i]; 
+			}
+			
+			delay_ms(2000);
+	}
+			LCD_display_string(int_to_char(distance));
+			delay_ms(10000);
     
     return 0;
 }
