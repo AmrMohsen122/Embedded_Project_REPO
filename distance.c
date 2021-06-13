b46c5fa517c586f94809cb8bdb98eab0cf98424e
amr mohamed <amrmohamed362363@gmail.com>
2021-06-13 19:23:54 +0200

    modified

--- distance.c
+++ distance.c
@@ -4,15 +4,29 @@
 #define ERAD 6371
 
 double degtorad(double deg) {
-	return (deg * PI / 180);
+    return (deg * PI / 180);
+}
+
+double convert(double in){
+    int mid = in / 100;
+    in -= mid *100;
+    in /= 60;
+    return mid + in;
 }
 
 
+
 // distance using haversine formula
-double calcDistance(double long1, double long2, double lat1, double lat2) {
+void calcDistance(double long11, double long22, double lat11, double lat22 , double * amount) {
+    double lat1 = convert(lat11);
+    double lat2 = convert(lat22);
+    double long1 = convert(long11);
+    double long2= convert(long22);
 
-	double x = pow(sin(degtorad(0.5 * (lat2 - lat1))), 2) + (cos(degtorad(lat1)) * cos(degtorad(lat2)) * pow(sin(degtorad(0.5 * (long2 - long1))), 2));
 
-	double d =  2 *ERAD*1000* asin(sqrt(x));
-	return d;
+
+    double x = pow(sin(degtorad(0.5 * (lat2 - lat1))), 2) + (cos(degtorad(lat1)) * cos(degtorad(lat2)) * pow(sin(degtorad(0.5 * (long2  - long1))), 2));
+
+    *amount =  2 ERAD1000* asin(sqrt(x));
+
 }
